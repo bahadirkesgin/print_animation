b459eb7be287e02daacd8d21f8e3fe17d985ef5e
Bahadır Utku Kesgin <bkesgin22@ku.edu.tr>
2022-12-15 12:04:14 +0300

    script_commit

---
+++ fancy_helloworld.py
@@ -0,0 +1,31 @@
+import random
+import string
+import time
+
+list_items = string.ascii_letters + " "
+user_input = str(input("Enter the text you want to write: "))
+txt_wri = user_input + " "
+i = 0
+new_txt = ""
+count = 0
+
+while i in range(0, len(txt_wri)):
+    a = random.choice(list_items)
+    if new_txt.__contains__(user_input):
+        print("\r", new_txt)
+        print("Number of steps: " + str(count))
+        break
+    elif a == txt_wri[i]:
+        new_txt = new_txt + txt_wri[i]
+        time.sleep(0.01)
+        i = i + 1
+        count = count + 1
+    else:
+        if i > 0:
+            print("\r", new_txt + a, end="")
+            time.sleep(0.005)
+            count = count + 1
+        else:
+            print("\r", a, end="")
+            time.sleep(0.007)
+            count = count + 1
